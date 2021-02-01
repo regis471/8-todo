f4b3ef79713a4eca68770996c65b60f843ad817e
regis471 <76951483+regis471@users.noreply.github.com>
2021-02-01 16:24:34 +0200

    Nepabaigta paskaita Todo

--- js/components/Todo.js
+++ js/components/Todo.js
@@ -10,7 +10,47 @@ class Todo {
     }
 
     add(text) {
-        this.list.push(text);
+        this.list.push({
+            text: text,
+            complete: false
+        });
+    }
+    edit(index, newText) {
+        this.list[index] = newText;
+    }
+
+    // uzduoties pasalinimas
+    remove(index){
+        const filtered = [];
+
+        for (let i=0; i<this.list.length; i++){
+            if (i !== index) {
+                filtered.push(this.list[i]);
+            }
+        }
+
+        //logic
+
+        this.list = unfiltered;
+    }
+    print (list) {
+        const printableList = list ? list : this.list;
+        console.table(printableList);
+    }
+
+    taskCompleted(index) {
+        this.list[index].completed = true;
+    }
+
+    printCompletedOnly() {
+        const completed = [];
+
+        for (const task of this.list) {
+            if (task.completed) {
+                completed.push(task);
+            }
+        }
+        this.print(completed);
     }
 }
 
--- js/main.js
+++ js/main.js
@@ -1,19 +1,46 @@
 import {Todo} from './components/Todo.js';
 
-const randomUzduotys = new Todo('Random uzduotys');
-const namuDarbai = new Todo('Namu darbai');
+const blynai = new Todo('Random uzduotys');
+
+blynai.add('Uzmaisyti tesla');
+blynai.add('Ijungti kaitlente');
+blynai.add('Istraukti keptuve');
+blynai.add('Ipilti tesla i ketuve');
+blynai.add('Apversti blynus');
+blynai.add('Isimti blynus');
+blynai.add('Isjungti kaitlente');
+
+blynai.edit(0, 'Isplauti dubeni ir uzmaisyti tesla');
+
+blynai.remove(4);
+blynai.remove(0);
+blynai.taskCompleted(0);
+
+blynai.summary();
+
+
+console.log(blynai.list);
+
+blynai.print();
+blynai.printCompletedOnly();
+
+
+
+
+
+//const namuDarbai = new Todo('Namu darbai');
 
 
 //console.log(randomUzduotys.summary());
-randomUzduotys.summary();
+//randomUzduotys.summary();
 
-randomUzduotys.add('Sukurti pirma uzduoti');
-namuDarbai.add('Susitvarkyti serloko projekta');
-console.log(namuDarbai);
+// randomUzduotys.add('Sukurti pirma uzduoti');
+// namuDarbai.add('Susitvarkyti serloko projekta');
+// console.log(namuDarbai);
 
-randomUzduotys.add('Uzmaisyti tesla');
-randomUzduotys.add('Nusiskusti');
-console.log(randomUzduotys);
+// randomUzduotys.add('Uzmaisyti tesla');
+// randomUzduotys.add('Nusiskusti');
+// console.log(randomUzduotys);
 
 
 // console.log(randomUzduotys.name);
@@ -26,4 +53,18 @@ console.log(randomUzduotys);
 //Namu darbai sarase yra 0 uzduociu.
 
 
+
+// default reiksmes = 1
+
+function daugyba(a = 1000, b = 1) {
+    return a * b;
+}
+
+console.log(daugyba (4, 5));
+console.log(daugyba (4));
+
+console.log(daugyba (2, 7));
+console.log(daugyba (2));
+
+console.log(daugyba ());
 
